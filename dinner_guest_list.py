ee90a9874b942205156d1105ec995ef8d16dcf5e
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-26 13:22:37 -0800

    Added pseudocode
    Might add more pseudocode if needed

---
+++ dinner_guest_list.py
@@ -0,0 +1,11 @@
+# Ask user for amount of guests they want to invite 
+
+# Greet the user
+
+# Ask user for guests
+
+# create loop that will keep apending names to the list until the limit is met
+
+# Ask user if they want to replace someone
+
+# Print the list
