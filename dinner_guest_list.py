f8b397d144b99697262cbc49a005fa88cbb19e3b
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-27 09:00:11 -0800

    Changed up pseudocode and asked user for number of guests and their names
    I also made function to sort the guest list

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -1,11 +1,21 @@
+# Create lists
+num_of_guests = []
+guests = []
+
+# function to print sorted list
+def print_sorted_list():
+    guests.sort()
+    print(guests)
+
+# function to replace someone
+
 # Ask user for amount of guests they want to invite 
+num_of_guests = int(input("How many guests do you want to invite?\n"))
 
-# Greet the user
+# Create loop that will keep apending names to the list until the limit is met
+for guest in range(num_of_guests):
+    guest = input(f"Enter the name of guest #{guest+1}")
+    guests.append(guest)
+print_sorted_list()
 
-# Ask user for guests
 
-# create loop that will keep apending names to the list until the limit is met
-
-# Ask user if they want to replace someone
-
-# Print the list
