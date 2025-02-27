558bf1d9aaa0e3e6aec4a81fce1d68661e9005e0
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-27 09:10:02 -0800

    Made some functions and tied them to eachother

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -1,20 +1,26 @@
 # Create lists
-num_of_guests = []
 guests = []
 
 # function to print sorted list
 def print_sorted_list():
     guests.sort()
-    print(guests)
-
+    for guest in guests:
+        print(guest)
+    choice = input("Do you want to replace someone? [y]es or [n]o? ")
+    if choice == "y":
+        replace()
+    else:
+        print("why not")
 # function to replace someone
+def replace():
+    print("hello")
 
 # Ask user for amount of guests they want to invite 
 num_of_guests = int(input("How many guests do you want to invite?\n"))
 
 # Create loop that will keep apending names to the list until the limit is met
 for guest in range(num_of_guests):
-    guest = input(f"Enter the name of guest #{guest+1}")
+    guest = input(f"Enter the name of guest #{guest+1}: ")
     guests.append(guest)
 print_sorted_list()
 
