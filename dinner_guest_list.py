c566211e7bae59693d396992854bb528a23169a8
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-28 11:10:11 -0800

    Cleaned up code and got rid of the replace()
    Instead of having a replace function I put   a remove and add function. If you accidently put a name on the list you can remove it. And if you forgot a name you can add it. If you want to replace someone you can use both of the functions

--- dinner_guest_list.py
+++ dinner_guest_list.py
@@ -8,10 +8,12 @@ def print_sorted_list():
         print(f"Here are the guests: {guest}")
     
     while True:
-        choice = input("\nDo you want to [R]eplace someone or send [I]]nvitations?").strip().title()
+        choice = input("\nDo you want to [R]emove or [A]dd a person or send [I]]nvitations?").strip().title()
         if choice == "R":
-            replace()
+            remove()
             break
+        elif choice == "A":
+            pass
         elif choice == "I":
             send_invitations()
             break
@@ -19,11 +21,11 @@ def print_sorted_list():
             print("Please enter a valid answer")
 
 # function to replace someone
-def replace():
+def remove():
     while True:
         try:
-            guests.remove(input("Who do you want to replace? "))
-            guests.append(input("Who's the new guest? "))
+            guests.remove(input("Who do you want to remove? ").strip().title())
+
             print_sorted_list()
             break
         except:
@@ -32,6 +34,7 @@ def replace():
 def send_invitations():
     for guest in guests:
         print(f"Hello {guest}. You have been invitated for dinner")
+    
     
 
 # Ask user for amount of guests they want to invite 
@@ -43,6 +46,6 @@ while True:
         print("Please enter a number")
 # Create loop that will keep apending names to the list until the limit is met
 for guest in range(num_of_guests):
-    guest = input(f"Enter the name of guest #{guest+1}: ")
+    guest = input(f"Enter the name of guest #{guest+1}: ").strip().title()
     guests.append(guest)
 print_sorted_list()
